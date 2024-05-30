ef9f7212a99c298b6c9bb64731860cb83cdd0ec4
J8rgen <jorgenvarjas@gmail.com>
2024-05-30 17:31:13 +0300

    singleton pattern, selected counter visual

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour{
+
+    public static Player Instance { get; private set; }
+
+
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged; //generics
+    public class OnSelectedCounterChangedEventArgs : EventArgs {
+        public ClearCounter selectedCounter;
+    }
 
     [SerializeField] private float moveSpeed = 7f; // [SerializeField] makes it editable in the editor under player in inspect
     [SerializeField] private GameInput gameInput;
@@ -10,30 +19,23 @@ public class Player : MonoBehaviour{
 
     private bool isWalking;
     private Vector3 lastInteractDir;
+    private ClearCounter selectedCounter;
+
+    private void Awake() {
+        if (Instance != null) {
+            Debug.LogError("There is more than one Player instance");
+        }
+        Instance = this;
+    }
+
 
     private void Start() {
         gameInput.OnInteractAction += GameInput_OnInteractAction;
     }
 
     private void GameInput_OnInteractAction(object sender, System.EventArgs e) {
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-
-        if (moveDir != Vector3.zero) { // if direction isnt 0 will contain last movement direction
-            lastInteractDir = moveDir; // even if we stop moving we will still use last movement direction for interact
-        }
-
-        float interactDistance = 2;
-        //if we hit something: // can also use raycastall if need an array of objects behind eachother
-        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask)) { //origin, direction, raycast hit, distance  (RC go to definition)
-            //Debug.Log(raycastHit.transform); //will see on the console if we hit sth
-            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) { //if interact hits ClearCounter
-                clearCounter.Interact();
-            }
-
-        }
-        else {
-            Debug.Log("-");
+        if(selectedCounter != null) {
+            selectedCounter.Interact();
         }
     }
 
@@ -59,13 +61,23 @@ public class Player : MonoBehaviour{
         //if we hit something: // can also use raycastall if need an array of objects behind eachother
         if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask)) { //origin, direction, raycast hit, distance  (RC go to definition)
             //Debug.Log(raycastHit.transform); //will see on the console if we hit sth
-            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) { //if interact hits ClearCounter
+
+            //if interact hits ClearCounter
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) { 
                 //clearCounter.Interact();
+                if (clearCounter != selectedCounter) { //kui ei ole juba selected siis nyyd on
+                    selectedCounter = clearCounter;
+
+                    SetSelectedCounter(clearCounter);
+                }
+            }
+            else {
+                SetSelectedCounter(null);
             }
 
         }
-        else {
-            Debug.Log("-");
+        else { // if nothing infront of player
+            SetSelectedCounter(null);
         }
     }
 
@@ -127,6 +139,15 @@ public class Player : MonoBehaviour{
         float roateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * roateSpeed); // with rotations use sleop, positions use lerp (takes a;b; t; choses a/b o inbweteen according to t value 0-1)
 
+    }
+
+    private void SetSelectedCounter(ClearCounter selectedCounter) {
+        this.selectedCounter = selectedCounter;
+
+
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs {
+            selectedCounter = selectedCounter //hover over to see which is which
+        });
     }
 
 }
