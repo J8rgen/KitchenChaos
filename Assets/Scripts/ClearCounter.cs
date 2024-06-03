c2a74dab05ac0bded62952f362e60fb7efc915e2
J8rgen <jorgenvarjas@gmail.com>
2024-06-03 22:47:35 +0300

    Player pick up, interface

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,29 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClearCounter : MonoBehaviour {
+public class ClearCounter : MonoBehaviour, IKitchenObjectParent { // monobehavior base class, interface
+
 
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
-    [SerializeField] private ClearCounter secondClearCounter;
-    [SerializeField] private bool testing;
+    //[SerializeField] private ClearCounter secondClearCounter;
+    //[SerializeField] private bool testing; testing
 
     private KitchenObject kitchenObject;
 
-    private void Update() {
-        if (testing && Input.GetKeyDown(KeyCode.T)) {
-            if (kitchenObject != null) {
-                kitchenObject.SetClearCounter(secondClearCounter); // set new parent for object
-            }
-        }
-    }
+    //private void Update() { OLD TESTING CODE
+    //    if (testing && Input.GetKeyDown(KeyCode.T)) {
+    //        if (kitchenObject != null) {
+    //            kitchenObject.SetKitchenObjectParent(secondClearCounter); // set new parent for object
+    //        }
+    //    }
+    //}
 
-    public void Interact() {
+    public void Interact(Player player) {
         if (kitchenObject == null) { // so we dont spawn infinite objects
             Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
         } else {
-            Debug.Log(kitchenObject.GetClearCounter());
+            //give the object to the player
+            kitchenObject.SetKitchenObjectParent(player);
         }
     }
 
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -6,30 +6,30 @@ public class KitchenObject : MonoBehaviour{
 
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
-    private ClearCounter clearCounter;
+    private IKitchenObjectParent kitchenObjectParent;
 
     public KitchenObjectSO GetKitchenObjectSO() {
         return kitchenObjectSO; 
     }
 
 
-    public void SetClearCounter(ClearCounter clearCounter) { // when we set clear counter to a different one    
-        if (this.clearCounter != null) {
-            this.clearCounter.ClearKitchenObject();
+    public void SetKitchenObjectParent(IKitchenObjectParent kitchenObjectParent) { // when we set clear counter to a different one    
+        if (this.kitchenObjectParent != null) {
+            this.kitchenObjectParent.ClearKitchenObject();
         }
 
-        this.clearCounter = clearCounter; //CAREFUL HERE WITH OLD AND NEW CLEAR COUNTER
+        this.kitchenObjectParent = kitchenObjectParent; //CAREFUL HERE WITH OLD AND NEW CLEAR COUNTER
 
-        if (clearCounter.HasKitchenObject()) {
-            Debug.LogError("Counter already has a KitchenObject");
+        if (kitchenObjectParent.HasKitchenObject()) {
+            Debug.LogError("IKitchenObjectParent already has a KitchenObject");
         }
-        clearCounter.SetKitchenObject(this);
+        kitchenObjectParent.SetKitchenObject(this);
 
-        transform.parent = clearCounter.GetKitchenObjectFollowTransform(); // will teleport the items after setting different one
+        transform.parent = kitchenObjectParent.GetKitchenObjectFollowTransform(); // will teleport the items after setting different one
         transform.localPosition = Vector3.zero;
     }
 
-    public ClearCounter GetClearCounter() {
-        return clearCounter;
+    public IKitchenObjectParent GetKitchenObjectParent() {
+        return kitchenObjectParent;
     }
 }
