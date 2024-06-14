6c3e9a1b0176379fb092a78324caec3cc5ddb6f6
J8rgen <jorgenvarjas@gmail.com>
2024-06-14 04:02:12 +0300

    Plate complete visual

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,9 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data.SqlTypes;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject {
+
+
+    public event EventHandler <OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs {
+        public KitchenObjectSO kitchenObjectSO;
+    
+    }
+
 
     [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
 
@@ -27,6 +36,11 @@ public class PlateKitchenObject : KitchenObject {
         }
         else {
             kitchenObjectSOList.Add(kitchenObjectSO);
+
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs {
+                kitchenObjectSO = kitchenObjectSO
+            }) ;
+
             return true;
         }
         //kitchenObjectSOList.Add(kitchenObjectSO);
